4766b8e6b4d883fe727da8642829d1232a5b8ed2
vin20784 <vin20784@gmail.com>
2020-04-04 01:37:43 +0530

    JS Promise need created

--- testapi.js
+++ testapi.js
@@ -1,42 +1,93 @@
 var unirest = require("unirest");
-var mysql      = require('mysql');
+var mysql   = require('mysql');
 
-var req = unirest("GET", "https://covid-193.p.rapidapi.com/statistics");
+//endpoint values are countries, statistics , countries : as per rapidapi
+ function EndPointSel(endpoint="statistics",queryvalue="india")
+{
+  const restep = "https://covid-193.p.rapidapi.com/" + endpoint;
+  var req = unirest("GET", restep);
 
+  var storedresp;
 
+  req.query({ "country" : queryvalue
+    //querykey , queryvalue
+    //"country": "india"
+  });
+  req.headers({
+    "x-rapidapi-host": "covid-193.p.rapidapi.com",
+    "x-rapidapi-key": "36191329b7mshcbbefaca44e0129p11f24djsn5d13d22f2b25"
+  });
+
+  req.end( function (res) {
+    if (res.error) throw new Error(res.error);
+
+    //storedresp =  res.json();
+    //storedresp = JSON.stringify(storedresp)
+    //storedresp = JSON.parse(storedresp)
+    console.log(res.body.response)
+    //return storedresp;
+    //return res.body.response;
+  });
+
+  return storedresp
+}
+ 
+function connecttodb()
+{
 var connection = mysql.createConnection({
   host     : '35.223.207.83',
   user     : 'root',
   password : 'npmgak@2020#'
 });
 
-
-req.query({
-	"country": "india"
-});
-req.headers({
-	"x-rapidapi-host": "covid-193.p.rapidapi.com",
-	"x-rapidapi-key": "36191329b7mshcbbefaca44e0129p11f24djsn5d13d22f2b25"
-});
-
- 
 connection.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
       return;
     }
-   
-    connection.query("CREATE DATABASE coviddb", function (err, result) {
-        if (err) throw err;
-        console.log("Database created");
-      });
-
-    req.end(function (res) {
-        if (res.error) throw new Error(res.error);
-        console.log(res.body.response);
-        connection.end();
-    });
-    //console.log('connected as id ' + connection.threadId);
+    console.log("Connected to DB!")
+    //return connection;
   });
 
+  connection.end();
+}
+
+
+
+
+
+
+
+/*liveCO.query("CREATE DATABASE coviddb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });*/
+
+
   
+    
+//console.log('connected as id ' + connection.threadId);
+
+// execution point
+
+function main()
+{
+
+  let response =  EndPointSel();
+
+  console.log(response);
+
+
+  let liveCO = connecttodb();
+
+  console.log(liveCO);
+
+  // how / should do i capture resp / connection object here?
+  //this is the best example to learn the async nature of JS 
+  // welcome to JS :) 
+  //liveCO.end();
+
+
+
+}
+main();
